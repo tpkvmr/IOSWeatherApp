7cdc2ae67ac3011cef9fddaadf1d35ed5d8b3d2b
tupikova_mariia <143123990+tpkvmr@users.noreply.github.com>
2025-12-04 18:00:12 +0200

    WeatherButton.swift

---
+++ WeatherButton.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
+
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButton(title: "Test Title",
+                      textColor: .white,
+                      backgroundColor: .blue)
+    }
+}
